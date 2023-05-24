d5d3e9ed51d2e62b39bf5d7e65744c07e11b3bf9
Sha Hos <107161530+ShaHos348@users.noreply.github.com>
2023-05-24 01:14:55 -0400

    Added businessForgotPassword backend

--- Backend/routes/businessUser.js
+++ Backend/routes/businessUser.js
@@ -2,6 +2,10 @@ const express = require("express");
 const connection = require("../connection");
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
+//const nodemailer = require("nodemailer");  not working currently
+require("dotenv").config();
+
 router.post("/businessSignup", (req, res) => {
   let user = req.body;
   query =
@@ -61,5 +65,70 @@ router.post("/businessSignup", (req, res) => {
     }
   );
 });
+
+router.post("/businessLogin", (req, res) => {
+  let user = req.body;
+  query = "SELECT * FROM businesslogin where username=? AND password=?";
+  connection.query(query, [user.username, user.password], (err, results) => {
+    if (!err) {
+      if (results.length == 1) {
+        const user = { business: results[0] };
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.status(200).json({ token: accessToken });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "Username and Password do not match!" });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+router.post("/businessForgotPassword", (req, res) => {
+  let user = req.body;
+  query = "SELECT username,password,email FROM businesslogin WHERE email=?";
+  connection.query(query, [user.email], (err, result) => {
+    if (!err) {
+      if (result.length == 1) {
+        return res.status(200).json({ message: result[0] });
+        /*var mailOptions = {
+          from: process.env.EMAIL,
+          to: result[0].email,
+          subject: "Username and Password for Junior Market LLC",
+          html: '<p><b>Your username is</b>' + result[0].username + '<br><b>Your password is</b>'+result[0].password+'<br><a href="http://localhost:4200/">Click here to login</a></p>'
+        };
+        transporter.sendMail(mailOptions,function(error,info){
+          if(!error){
+            console.log('email sent: '+info.response);
+          }
+          else{
+            console.log(error);
+          }
+        });
+        return res.status(200).json({ message: "Email sent succesfully!" }); */ // Not working currently
+      } else {
+        return res
+          .status(400)
+          .json({
+            message: "This email is not associated with any business user!",
+          });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+/*var transporter = nodemailer.createTransport({ 
+  service: 'gmail',
+  auth:{
+    user: process.env.EMAIL,
+    pass: process.nextTick.PASSWORD
+  }
+}) */ // Not working currently
 
 module.exports = router;
